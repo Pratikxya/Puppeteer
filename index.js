0485dea61826807f4381ed5c2f30d919645c158d
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-10-21 16:59:54 +0545

    feat: :art: Added login feature.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import Puppeteer from "puppeteer";
+import dotenv from "dotenv";
+
+dotenv.config();
+const username = process.env.MYNAME;
+const password = process.env.PASSWORD;
 
 (async () => {
   const browser = await Puppeteer.launch({ headless: false });
@@ -7,8 +12,6 @@ import Puppeteer from "puppeteer";
 
   await page.click("a[href='/login']");
 
-  await page.type("#username", "admin", { delay: 100 });
-  await page.type("#password", "admin", { delay: 100 });
-
-  //   await browser.close();
+  await page.type("#username", username);
+  await page.type("#password", password);
 })();
