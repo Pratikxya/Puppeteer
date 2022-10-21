cfe21f2def09a9f87a7298eae0a5c8e768f70530
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-10-21 17:12:10 +0545

    feat: :art: Getting the dimensions of web-page opened

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import Puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await Puppeteer.launch({ headless: false });
+  const browser = await Puppeteer.launch();
   const page = await browser.newPage();
-  // await page.setDefaultNavigationTimeout(0);
-  await page.goto("https://www.collegenote.net/courses/TU/CSIT/", {
-    waitUntil: "load",
-    // Remove the timeout
-    timeout: 0,
+  await page.goto("https://www.collegenote.net/courses/TU/CSIT/");
+
+  const getDimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    };
   });
-  await page.pdf({ path: "Collegenotes.pdf", format: "A4" });
+
+  console.log(getDimensions);
+
   await browser.close();
 })();
