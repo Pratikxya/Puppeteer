c3366e79ca221448b9d0a85e6c298e6d8957f0be
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-10-21 14:36:37 +0545

    feat: :art: Added click feature.

--- index.js
+++ index.js
@@ -4,25 +4,8 @@ import Puppeteer from "puppeteer";
   const browser = await Puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://quotes.toscrape.com/");
-  //   await page.screenshot({ path: "example.png" });
 
-  const grabQuotes = await page.evaluate(() => {
-    const quotes = document.querySelectorAll(".quote");
-    let quotesArr = [];
-    quotes.forEach((quoteTag) => {
-      const quoteInfo = quoteTag.querySelectorAll("span");
-      const actualQuote = quoteInfo[0];
-      const actualAuthor = quoteInfo[1];
+  await page.click("a[href='/login']");
 
-      const authorName = actualAuthor.querySelector("small");
-
-      quotesArr.push({
-        quote: actualQuote.innerText,
-        author: authorName.innerText,
-      });
-    });
-    return quotesArr;
-  });
-  console.log(grabQuotes);
-  await browser.close();
+  //   await browser.close();
 })();
