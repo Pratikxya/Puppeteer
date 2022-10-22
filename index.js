c733bd1f088f8f742cdfd7f5563ef5d7409dcc3e
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-10-22 17:06:40 +0545

    feat: :art: Extracted link title and date from each notice.

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-//packages
 import Puppeteer from "puppeteer";
 import axios from "axios";
 import cheerio from "cheerio";
-
 const url = "https://www.tuiost.edu.np/notices";
-
 async function scrape() {
   //fetching data
   const data = await axios.get(url);
-
   //loading data into cheerio
   const $ = cheerio.load(data.data);
-
   const listItems = $(".mt-3");
   console.log(listItems.length);
   listItems.each(function (idx, el) {
-    const title = $(el).children("a").text();
+    const title = $(el).children("a").children("b").text();
     const date = $(el).children("small").text();
     const link = $(el).children("a").attr("href");
 
-    console.log(title);
-    console.log(date);
-    console.log(link);
+    const notices = {
+      title,
+      date,
+      link,
+    };
+    console.log(notices);
   });
 }
 scrape();
